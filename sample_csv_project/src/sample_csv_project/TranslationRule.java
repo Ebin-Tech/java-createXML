1c8b678e3c0492858ee4d795148943962613b97a
Ebin Benny Thomas <82931172+Ebin-Tech@users.noreply.github.com>
2022-06-28 23:56:27 -0700

    Add files via upload

---
+++ sample_csv_project/src/sample_csv_project/TranslationRule.java
@@ -0,0 +1,37 @@
+package sample_csv_project;
+
+public class TranslationRule {
+
+	private String name;
+	private String inherit;
+	private String translate;
+	private String updateDestinationLanguage;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getInherit() {
+		return inherit;
+	}
+	public void setInherit(String inherit) {
+		this.inherit = inherit;
+	}
+	public String getTranslate() {
+		return translate;
+	}
+	public void setTranslate(String translate) {
+		this.translate = translate;
+	}
+	public String getUpdateDestinationLanguage() {
+		return updateDestinationLanguage;
+	}
+	public void setUpdateDestinationLanguage(String updateDestinationLanguage) {
+		this.updateDestinationLanguage = updateDestinationLanguage;
+	}
+	
+	
+	
+	
+}
